a9a743b895f496e16ae916d691ba92ec7183888a
Rafael Oliveira <oliveirafael@icloud.com>
2021-01-02 15:42:44 -0300

    create func getService

---
+++ CharactersOfStarWars/Services/Webservice.swift
@@ -0,0 +1,21 @@
+//
+//  Webservice.swift
+//  CharactersOfStarWars
+//
+//  Created by Rafael Oliveira on 02/01/21.
+//
+
+import Foundation
+class Webservice {
+    // criar função que retorna os dados da API (getfunc)
+    func getService(url: URL, completion:@escaping(Character?)->()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(nil)
+                print(error.localizedDescription)
+            } else if let data = data {
+                print(data)
+            }
+        }.resume()
+    }
+}
