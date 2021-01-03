a64ac0e8d57e2562009f1b34fe06c1322e6fbb9b
Rafael Oliveira <oliveirafael@icloud.com>
2021-01-03 09:47:19 -0300

    reload ViewController After receive data from API using getService;

--- CharactersOfStarWars/Controllers/CharacterViewController.swift
+++ CharactersOfStarWars/Controllers/CharacterViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 class CharacterViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    var CharacterVM: CharacterViewModel!
     override func viewDidLoad() {
         setup()
+        
     }
+    
     
     private func setup(){
         navigationController?.navigationBar.prefersLargeTitles = true
         let url = URL(string: "https://swapi.dev/api/people/1/")!
-        Webservice().getService(url: url) { _ in
-            
+        Webservice().getService(url: url) { character in
+            if let character = character {
+                self.CharacterVM = CharacterViewModel(character)
+                DispatchQueue.main.async {
+                    self.nameLabel.text = self.CharacterVM.name
+                    self.viewWillAppear(true)
+                }
+//                TODO: Colocar o dado no label antes dele carregar
+                print(self.CharacterVM.name)
+            }
         }
     }
 }
