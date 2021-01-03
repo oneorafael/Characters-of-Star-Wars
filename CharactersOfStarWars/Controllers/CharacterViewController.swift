f9d26b8579000ca2fb8c854b74cf598a04c512c1
Rafael Oliveira <oliveirafael@icloud.com>
2021-01-03 13:11:59 -0300

    create a button to activate getService function and randomize return data

--- CharactersOfStarWars/Controllers/CharacterViewController.swift
+++ CharactersOfStarWars/Controllers/CharacterViewController.swift
@@ -7,27 +7,43 @@
 
 import UIKit
 class CharacterViewController: UIViewController {
+//    MARK: - IBoutlets
     @IBOutlet weak var nameLabel: UILabel!
+    
     var CharacterVM: CharacterViewModel!
     override func viewDidLoad() {
         setup()
-        
     }
     
+//    MARK: - Functions
     
+    /* func setup()
+     * set large title
+     * get dat from api
+     * reload viewController to aply data
+     */
     private func setup(){
+        
         navigationController?.navigationBar.prefersLargeTitles = true
-        let url = URL(string: "https://swapi.dev/api/people/1/")!
+        //TODO: limite de variações de pessoas é de 83
+        var random = Int.random(in: 0...83)
+        let url = URL(string: "https://swapi.dev/api/people/\(random)/")!
+        
         Webservice().getService(url: url) { character in
             if let character = character {
                 self.CharacterVM = CharacterViewModel(character)
                 DispatchQueue.main.async {
                     self.nameLabel.text = self.CharacterVM.name
+                    self.title = self.CharacterVM.name
                     self.viewWillAppear(true)
                 }
-//                TODO: Colocar o dado no label antes dele carregar
                 print(self.CharacterVM.name)
             }
         }
     }
+    
+    @IBAction func reloadCharacterButtonPressed(_ sender: UIButton) {
+        setup()
+    }
+    
 }
